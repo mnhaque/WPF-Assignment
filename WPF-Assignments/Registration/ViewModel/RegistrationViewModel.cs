fc1c43d8f5e91a01313c9a6487b813bf1ad145d1
Nasim <mahammed_haque@epam.com>
2018-02-27 09:13:00 +0530

    created solution and project

---
+++ WPF-Assignments/Registration/ViewModel/RegistrationViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registration.ViewModel
+{
+    public class RegistrationViewModel : INotifyPropertyChanged
+    {
+        private string name;
+        private string dob;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+        public string DOB
+        {
+            get
+            {
+                return this.dob;
+            }
+            set
+            {
+                dob = value;
+                RaisePropertyChanged(nameof(DOB));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
